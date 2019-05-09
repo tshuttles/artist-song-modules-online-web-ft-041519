b3b599ae816477ec996fa627f0848af6a827eba3
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 16:24:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@all = []
+  @@songs = []
 
   def initialize
-    @@all << self
+    @@songs << self
   end
 
   # def self.find_by_name(name)
@@ -17,7 +17,7 @@ class Song
   # end
 
   def self.all
-    @@all
+    @@songs 
   end
 
   def artist=(artist)
