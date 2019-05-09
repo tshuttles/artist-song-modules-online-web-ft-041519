a6fe34f05459a0359c5dc66da6e44624ce2c46fd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-09 15:39:29 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable 
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
   
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-  
+    def self.count
+      self.all.count
+    end
+  end 
 end 
